f16d97475bd91cc2e18893699c28189f39a35b74
Hendrik Gani <hendrik.gani@gmail.com>
2018-08-26 15:42:44 +1000

    fix text

--- app/javascript/components/modal-vote.vue
+++ app/javascript/components/modal-vote.vue
@@ -4,11 +4,11 @@
       .modal-content
         .modal-body
           p
-            | If this Target gets approved by the Project admin, the admin will withdraw your ETH to fund the project. Please do your due dilligence to make sure the admin can be trusted. 
+            | If this Target gets approved by the Project admin, the admin will be able to withdraw your ETH to fund the project. Please do your due diligence to make sure the admin can be trusted. 
           .form-group
             input.form-control(type="number" step="0.001" v-model="voteLocal.fund")
           p
-            | You can cancel your vote and get your ETH back prior to approval.           
+            | You can cancel your vote and get your ETH back prior to approval.
         .modal-footer
           button.btn.btn-primary(@click="submit()") OK
           button.btn.btn-secondary(data-dismiss="modal" @click="cancel()")  Cancel
@@ -54,4 +54,3 @@ export default {
   }
 };
 </script>
-
